5bd4f15c58b64042c897ed1f06105a833280507f
donnebiyu0 <nebiyuab00@gmail.com>
2023-12-30 06:46:58 -0800

    mongo fix

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -25,7 +25,7 @@ const EventSchema = new Schema({
   startDate: { type: Date, default: Date.now },
   endDate: { type: Date, default: Date.now },
   price: { type: String },
-  isFree: { type: String, default: false },
+  isFree: { type: Boolean, default: false },
   url: { type: String },
   category: { type: Schema.Types.ObjectId, ref: "Category" },
   organizer: { type: Schema.Types.ObjectId, ref: "User" },
